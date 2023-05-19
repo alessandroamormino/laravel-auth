b2f184ac77e171461dce56bb79b8f990923b6572
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-19 21:43:15 +0200

    feat: add homepage with styled buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts/main')
 
 @section('content')
-  Homepage
+<main>
+  <div class="container-centered">
+    <h1>Hi, I'm Alessandro Amormino</h1>
+    <h3>Full Stack Web Developer</h3>
+    <div class="buttons">
+      <button class="_btn _btn-primary">View works</button>
+      <button class="_btn _btn-secondary">Contact me</button>
+    </div>
+  </div>
+</main>
 @endsection
